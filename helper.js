de6dba736fcd53f6ec8342f954622543ebcdf8d3
encoder59 <46228288+encoder59@users.noreply.github.com>
2018-12-28 21:38:11 +0100

    Create helper.js

---
+++ helper.js
@@ -0,0 +1,32 @@
+// Enable pusher logging - don't include this in production
+Pusher.logToConsole = false;
+
+var pusher = new Pusher('ca47239ee315d662e6f1', {
+  cluster: 'eu',
+  forceTLS: true
+});
+
+var channel = pusher.subscribe('my-channel');
+channel.bind('my-event', function(data) {
+  render('my-event', data);
+});
+
+channel.bind('my-alert', function(data) {
+  render('my-alert', data);
+});
+
+function render(msg, data){
+  var ctr = document.getElementById(msg+'s');
+  var tpl = document.getElementById(msg+'-tpl').cloneNode(true);
+  tpl.removeAttribute("id");
+  var from = tpl.getElementsByClassName("ws-data-from")[0];
+  from.innerText = data.name;
+  var msg = tpl.getElementsByClassName("ws-data-body")[0];
+  msg.innerText = data.message;
+  if( ctr.childNodes.length == 0) {
+    ctr.appendChild(tpl);
+  } else {
+    ctr.insertBefore(tpl, ctr.childNodes[0])
+  }
+  $('.toast').toast('show');
+}
