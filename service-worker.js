04524d35f56462cc7db73edba0c39f70b94fb868
encoder59 <46228288+encoder59@users.noreply.github.com>
2019-02-15 18:12:43 +0100

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -14,3 +14,20 @@ self.addEventListener('message', function(event){
     console.log("SW Received Message: " + event.data);
     event.ports[0].postMessage("SW Says 'Hello back!'");
 });
+
+function send_message_to_client(client, msg){
+    return new Promise(function(resolve, reject){
+        var msg_chan = new MessageChannel();
+
+        msg_chan.port1.onmessage = function(event){
+            if(event.data.error){
+                reject(event.data.error);
+            }else{
+                resolve(event.data);
+                console.log(event.data);
+            }
+        };
+
+        client.postMessage("SW Says: '"+msg+"'", [msg_chan.port2]);
+    });
+}
